a8481edbb01efebaf05fd5c935dd0c9d3ceb2d81
LisaCee <lisaceedesign@gmail.com>
2019-08-06 19:07:22 -0700

    images all same size, bootstrap nav started

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import Card from "./Card";
 import "../styles/gallery.css";
 
-import { Container } from 'react-bootstrap';
-
+import { Container, Row, Col } from "react-bootstrap";
 
 const Gallery = () => {
     return (
         <Container>
-            <div className="gallery">
-                <Card image={require("../images/movie-time-machine.jpg")} />
-                <Card image={require("../images/book-finder.jpg")} />
-                <Card image={require("../images/knowledge-rocket.jpg")} />
-                <Card image={require("../images/lambda-notes.jpg")} />
-            </div>
+            <Row>
+                <Col>
+                    <div className="gallery">
+                        <a href="https://movie-time-machine.netlify.com/" target="_">
+                            <Card image={require("../images/movie-time-machine.jpg")} />
+                            <div className="details">
+                                <p>candy</p>
+                            </div>
+                        </a>
+                        <a href="https://lisacee-book-finder.netlify.com/" target="_">
+                            <Card image={require("../images/book-finder.jpg")} />
+                        </a>
+                        <a href="https://infallible-euler-24eb8a.netlify.com/" target="_">
+                            <Card image={require("../images/knowledge-rocket.jpg")} />
+                        </a>
+                        <a href="https://infallible-euler-24eb8a.netlify.com/">
+                            <Card image={require("../images/lambda-notes.jpg")} />
+                        </a>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "../styles/header.css";
+import { Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div id="header">
-            <img
-                className="logo"
-                src="https://sedonatrailzen.com/wp-content/uploads/2016/01/Enso_grey.png"
-                alt="placeholder logo"
-            />
-            <h1>LisaCee</h1>
-        </div>
-    );
+  return (
+    <div id="header">
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
+
+// {/* <div id="header">
+//       {/* <Logo /> */}
+//       <img
+//         className="logo"
+//         src={require("../images/LisaCee.png")}
+//         alt="placeholder logo"
+//       />
+//       <h1>Lisa Canini</h1>
+//       <p>
+//         Full stack developer passionate about making easy-to-use, accessible and
+//         responsive user interfaces
+//       </p>
+//     </div> */}
