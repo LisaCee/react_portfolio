989407ecaee99e7d9969fac01cc2707276625ea3
LisaCee <lisaceedesign@gmail.com>
2019-08-09 15:42:50 -0700

    formatting icons

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,70 +1,65 @@
-import React from "react";
-import Card from "./Card";
-import "../styles/gallery.css";
+import React from 'react';
+import Card from './Card';
+import '../styles/gallery.css';
 
-import netlify from '../images/netlify.png';
+import movieDB from '../images/movieDB.png';
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Gallery = () => {
-    return (
-        <Container>
-            {/* <Row>
-        <Col> */}
-            <div className="gallery">
-                <a href="https://movie-time-machine.netlify.com/" target="_">
-                    <Card image={require("../images/movie-time-machine.jpg")} />
-                </a>
-                <div className="details">
-                    <i className="fab fa-react" />
-                    <i className="fab fa-css3"></i>
-                    <i className="fab fa-bootstrap"></i>
-                    <i className="fab fa-github"></i>
-                    <img src="https://www.themoviedb.org/assets/2/v4/logos/312x276-primary-blue-fb50dee3bf664c866fd216e6cee64af33d20707ea3091ddc65c5e8aa4c152eb2.png" alt="The movie DB" id="moviedb" />
-                    <img src={netlify} alt="netlify" id="netlify" />
-                </div>
-
-                <a href="https://lisacee-book-finder.netlify.com/" target="_">
-                    <Card image={require("../images/book-finder.jpg")} />
-                </a>
-                <div className="details">
-                    <i className="fab fa-react" />
-                    <i className="fab fa-css3"></i>
-                    <i className="fab fa-bootstrap"></i>
-                    <i className="fab fa-google"></i>
-                    <i className="fab fa-github"></i>
-
-                    <img src={netlify} alt="netlify" id="netlify" />
-                </div>
-
-                <a href="https://infallible-euler-24eb8a.netlify.com/" target="_">
-                    <Card image={require("../images/knowledge-rocket.jpg")} />
-                </a>
-                <div className="details">
-                    <i className="fab fa-react" />
-                    <i className="fab fa-css3"></i>
-                    <i className="fab fa-bootstrap"></i>
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-python"></i>
-                    <img src={netlify} alt="netlify" id="netlify" />
-                </div>
-
-                <a href="https://infallible-euler-24eb8a.netlify.com/">
-                    <Card image={require("../images/lambda-notes.jpg")} />
-                </a>
-                <div className="details">
-                    <i className="fab fa-react" />
-                    <i className="fab fa-less"></i>
-                    <i className="fab fa-bootstrap"></i>
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-python"></i>
-                    <img src={netlify} alt="netlify" id="netlify" />
-                </div>
+  return (
+    <Container>
+      <div className='gallery'>
+        <Row>
+          <Col>
+            <a href='https://movie-time-machine.netlify.com/' target='_'>
+              <Card image={require('../images/movie-time-machine.jpg')} />
+            </a>
+            <div className='details'>
+              <i className='fab fa-react fa-2x' />
+              <i className='fab fa-css3 fa-2x' />
+              <i className='fab fa-bootstrap fa-2x' />
+              <i className='fab fa-github fa-2x' />
+              <img src={movieDB} alt='The movie DB' id='moviedb' />
             </div>
-            {/* </Col>
-      </Row> */}
-        </Container>
-    );
+
+            <a href='https://lisacee-book-finder.netlify.com/' target='_'>
+              <Card image={require('../images/book-finder.jpg')} />
+            </a>
+            <div className='details'>
+              <i className='fab fa-react' />
+              <i className='fab fa-css3' />
+              <i className='fab fa-bootstrap' />
+              <i className='fab fa-google' />
+              <i className='fab fa-github' />
+            </div>
+
+            <a href='https://infallible-euler-24eb8a.netlify.com/' target='_'>
+              <Card image={require('../images/knowledge-rocket.jpg')} />
+            </a>
+            <div className='details'>
+              <i className='fab fa-react' />
+              <i className='fab fa-css3' />
+              <i className='fab fa-bootstrap' />
+              <i className='fab fa-github' />
+              <i className='fab fa-python' />
+            </div>
+
+            <a href='https://infallible-euler-24eb8a.netlify.com/'>
+              <Card image={require('../images/lambda-notes.jpg')} />
+            </a>
+            <div className='details'>
+              <i className='fab fa-react' />
+              <i className='fab fa-less' />
+              <i className='fab fa-bootstrap' />
+              <i className='fab fa-github' />
+              <i className='fab fa-python' />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
 };
 
 export default Gallery;
