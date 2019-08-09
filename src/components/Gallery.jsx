2ca88463e69242f688c2e467f734b03b026b60db
LisaCee <lisaceedesign@gmail.com>
2019-08-08 18:51:28 -0700

    still working on bps, added font-awesome icons

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,32 +2,67 @@ import React from "react";
 import Card from "./Card";
 import "../styles/gallery.css";
 
+import netlify from '../images/netlify.png';
+
 import { Container, Row, Col } from "react-bootstrap";
 
 const Gallery = () => {
     return (
         <Container>
-            <Row>
-                <Col>
-                    <div className="gallery">
-                        <a href="https://movie-time-machine.netlify.com/" target="_">
-                            <Card image={require("../images/movie-time-machine.jpg")} />
-                            <div className="details">
-                                <p>candy</p>
-                            </div>
-                        </a>
-                        <a href="https://lisacee-book-finder.netlify.com/" target="_">
-                            <Card image={require("../images/book-finder.jpg")} />
-                        </a>
-                        <a href="https://infallible-euler-24eb8a.netlify.com/" target="_">
-                            <Card image={require("../images/knowledge-rocket.jpg")} />
-                        </a>
-                        <a href="https://infallible-euler-24eb8a.netlify.com/">
-                            <Card image={require("../images/lambda-notes.jpg")} />
-                        </a>
-                    </div>
-                </Col>
-            </Row>
+            {/* <Row>
+        <Col> */}
+            <div className="gallery">
+                <a href="https://movie-time-machine.netlify.com/" target="_">
+                    <Card image={require("../images/movie-time-machine.jpg")} />
+                </a>
+                <div className="details">
+                    <i className="fab fa-react" />
+                    <i className="fab fa-css3"></i>
+                    <i className="fab fa-bootstrap"></i>
+                    <i className="fab fa-github"></i>
+                    <img src="https://www.themoviedb.org/assets/2/v4/logos/312x276-primary-blue-fb50dee3bf664c866fd216e6cee64af33d20707ea3091ddc65c5e8aa4c152eb2.png" alt="The movie DB" id="moviedb" />
+                    <img src={netlify} alt="netlify" id="netlify" />
+                </div>
+
+                <a href="https://lisacee-book-finder.netlify.com/" target="_">
+                    <Card image={require("../images/book-finder.jpg")} />
+                </a>
+                <div className="details">
+                    <i className="fab fa-react" />
+                    <i className="fab fa-css3"></i>
+                    <i className="fab fa-bootstrap"></i>
+                    <i className="fab fa-google"></i>
+                    <i className="fab fa-github"></i>
+
+                    <img src={netlify} alt="netlify" id="netlify" />
+                </div>
+
+                <a href="https://infallible-euler-24eb8a.netlify.com/" target="_">
+                    <Card image={require("../images/knowledge-rocket.jpg")} />
+                </a>
+                <div className="details">
+                    <i className="fab fa-react" />
+                    <i className="fab fa-css3"></i>
+                    <i className="fab fa-bootstrap"></i>
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-python"></i>
+                    <img src={netlify} alt="netlify" id="netlify" />
+                </div>
+
+                <a href="https://infallible-euler-24eb8a.netlify.com/">
+                    <Card image={require("../images/lambda-notes.jpg")} />
+                </a>
+                <div className="details">
+                    <i className="fab fa-react" />
+                    <i className="fab fa-less"></i>
+                    <i className="fab fa-bootstrap"></i>
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-python"></i>
+                    <img src={netlify} alt="netlify" id="netlify" />
+                </div>
+            </div>
+            {/* </Col>
+      </Row> */}
         </Container>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "../styles/header.css";
+import Logo from "../images/LisaCee.png";
 import { Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
   return (
     <div id="header">
-      <Navbar expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+      <Navbar expand="md">
+        <Navbar.Brand href="#home" className="d-inline align-top">
+          <img src={Logo} alt="logo" />
+          <h3>Lisa Canini</h3>
+          <br />
+          <p>
+            Full stack developer passionate about making easy-to-use, accessible
+            and responsive user interfaces
+          </p>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" id="nav-button" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#bio">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -20,17 +30,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// {/* <div id="header">
-//       {/* <Logo /> */}
-//       <img
-//         className="logo"
-//         src={require("../images/LisaCee.png")}
-//         alt="placeholder logo"
-//       />
-//       <h1>Lisa Canini</h1>
-//       <p>
-//         Full stack developer passionate about making easy-to-use, accessible and
-//         responsive user interfaces
-//       </p>
-//     </div> */}
