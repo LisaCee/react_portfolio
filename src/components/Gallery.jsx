e8e6f5092cb06d5e87de94a7365d3cce84012e26
LisaCee <lisaceedesign@gmail.com>
2019-08-13 19:00:40 -0700

    updates from codesandbox

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -16,44 +16,47 @@ const Gallery = () => {
               <Card image={require('../images/movie-time-machine.jpg')} />
             </a>
             <div className='details'>
-              <i className='fab fa-react fa-2x' />
-              <i className='fab fa-css3 fa-2x' />
-              <i className='fab fa-bootstrap fa-2x' />
-              <i className='fab fa-github fa-2x' />
+              <i className='fab fa-react fa-lg' />
+              <i className='fab fa-css3 fa-lg' />
+              <i className='fab fa-bootstrap fa-lg' />
+              <i className='fab fa-github fa-lg' />
               <img src={movieDB} alt='The movie DB' id='moviedb' />
             </div>
-
+          </Col>
+          <Col>
             <a href='https://lisacee-book-finder.netlify.com/' target='_'>
               <Card image={require('../images/book-finder.jpg')} />
             </a>
             <div className='details'>
-              <i className='fab fa-react fa-2x' />
-              <i className='fab fa-css3 fa-2x' />
-              <i className='fab fa-bootstrap fa-2x' />
-              <i className='fab fa-google fa-2x' />
-              <i className='fab fa-github fa-2x' />
+              <i className='fab fa-react fa-lg' />
+              <i className='fab fa-css3 fa-lg' />
+              <i className='fab fa-bootstrap fa-lg' />
+              <i className='fab fa-google fa-lg' />
+              <i className='fab fa-github fa-lg' />
             </div>
-
+          </Col>
+          <Col>
             <a href='https://infallible-euler-24eb8a.netlify.com/' target='_'>
               <Card image={require('../images/knowledge-rocket.jpg')} />
             </a>
             <div className='details'>
-              <i className='fab fa-react fa-2x' />
-              <i className='fab fa-css3 fa-2x' />
-              <i className='fab fa-bootstrap fa-2x' />
-              <i className='fab fa-github fa-2x' />
-              <i className='fab fa-python fa-2x' />
+              <i className='fab fa-react fa-lg' />
+              <i className='fab fa-css3 fa-lg' />
+              <i className='fab fa-bootstrap fa-lg' />
+              <i className='fab fa-github fa-lg' />
+              <i className='fab fa-python fa-lg' />
             </div>
-
+          </Col>
+          <Col>
             <a href='https://infallible-euler-24eb8a.netlify.com/'>
               <Card image={require('../images/lambda-notes.jpg')} />
             </a>
             <div className='details'>
-              <i className='fab fa-react fa-2x' />
-              <i className='fab fa-less fa-2x' />
-              <i className='fab fa-bootstrap fa-2x' />
-              <i className='fab fa-github fa-2x' />
-              <i className='fab fa-python fa-2x' />
+              <i className='fab fa-react fa-lg' />
+              <i className='fab fa-less fa-lg' />
+              <i className='fab fa-bootstrap fa-lg' />
+              <i className='fab fa-github fa-lg' />
+              <i className='fab fa-python fa-lg' />
             </div>
           </Col>
         </Row>
