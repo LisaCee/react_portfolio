ef22e35dc05629fa27d4e98a1d341e3deef0e7ee
LisaCee <lisaceedesign@gmail.com>
2019-08-15 17:22:04 -0700

    codesandbox and formatting form

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,20 +9,30 @@ const About = () => {
       <Container>
         <h1>Hi. I'm Lisa Cee.</h1>
         <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget
+          convallis nulla, in pulvinar nulla. Suspendisse tincidunt consequat
+          ipsum vel pulvinar. Morbi at euismod nisi, quis vestibulum turpis. In
+          in volutpat ipsum, sit amet commodo odio. Etiam sed dui ac quam
+          porttitor finibus. Duis id vulputate lorem. In pharetra dui vitae
+          ipsum consequat, non dictum quam vestibulum. In luctus aliquet nulla,
+          in finibus est.
+        </p>
+
+        {/* <p>
           I am a full stack developer passionate about creating websites that
           remove barriers to accessing information and make that information
           readily available to everyone. I am experienced with
           HTML/CSS/JavaScript, React, MongoDB, Node and more after completing an
           intensive 13-month mastery-based program at Lambda School.
-        </p>
+        </p> */}
         <div>
           <h3>Skills</h3>
-          <p>
+          {/* <p>
             Lisa is a valuable member of any team. She has great curiosity and
             the resolve to find solutions. While her focus is in front end
             development, Lisa has a full stack education in order to better
             understand what makes a site work and how she can make it better.
-          </p>
+          </p> */}
           <ul>
             <li className='fab fa-react'>
               <span className='icon'>React</span>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,73 +1,70 @@
 import React from 'react';
 import '../styles/contact.css';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 const Contact = () => {
   return (
     <Container>
-      <h3>Contact Me</h3>
-      <p>
-        Whether you wish to work with me or just want to chat about Prince or
-        programming, I'd love to hear from you.
-      </p>
-      <form action='https://formspree.io/lisaceedesign@gmail.com' method='POST'>
+      <Form action='https://formspree.io/lisaceedesign@gmail.com' method='POST'>
+        <h3>Contact Me</h3>
+        <p>
+          Whether you wish to work with me or just want to chat about Prince or
+          programming, I'd love to hear from you.
+        </p>
         <Row>
-          <Col xs md={6}>
-            <input
-              type='text'
-              name='name'
-              id='name'
-              placeholder='Name'
-              required
-            />
-          </Col>
-          <Col xs md={6}>
-            <input
-              type='hidden'
-              name='_next'
-              value='http://127.0.0.1:5500/index.html'
-            />
-            <input
-              type='email'
-              name='email'
-              id='email'
-              placeholder='Email'
-              required
-            />
-          </Col>
-          <Col>
-            <input
-              type='text'
-              name='subject'
-              id='subject'
-              placeholder='Subject'
-            />
-          </Col>
-          <Col>
-            <textarea
-              name='message'
-              id='message'
-              placeholder='Message'
-              rows='6'
-            />
-          </Col>
-          <Col>
-            <ul class='actions'>
-              <li>
-                <input
-                  type='submit'
-                  class='primary submit'
-                  value='Send Message'
-                />
-              </li>
-              <li>
-                <input type='reset' value='Reset Form' />
-              </li>
-            </ul>
-          </Col>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Name'
+            required
+          />
         </Row>
-      </form>
+        <Row>
+          <input
+            type='hidden'
+            name='_next'
+            value='http://127.0.0.1:5500/index.html'
+          />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+            required
+          />
+
+          <input
+            type='text'
+            name='subject'
+            id='subject'
+            placeholder='Subject'
+          />
+
+          <textarea
+            name='message'
+            id='message'
+            placeholder='Message'
+            rows='6'
+          />
+
+          <div class='actions'>
+            <Button
+              type='submit'
+              class='primary submit'
+              value='Send Message'
+              block
+            >
+              Submit
+            </Button>
+
+            <Button type='reset' value='Reset Form' block>
+              Reset
+            </Button>
+          </div>
+        </Row>
+      </Form>
     </Container>
   );
 };
