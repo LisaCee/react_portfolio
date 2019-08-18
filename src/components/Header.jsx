7af14da2e0fef0d0b47ce4c2efd7b61180f719c4
LisaCee <lisaceedesign@gmail.com>
2019-08-18 16:17:28 -0700

    header colors and sm bp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import '../styles/header.css';
 import Logo from '../images/LisaCee.png';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Row, Col } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <div id='header'>
       <Navbar expand='md'>
         <Navbar.Brand href='#home' className='d-inline align-top'>
-          <img src={Logo} alt='logo' />
-          <h3>Lisa Canini</h3>
-          <br />
-          {/* <p>
-            Full stack developer passionate about making easy-to-use, accessible
-            and responsive user interfaces
-          </p> */}
+          <Row>
+            <Col xs={3}>
+              <img src={Logo} alt='logo' />
+            </Col>
+            <Col>
+              <h3>Lisa Canini</h3>
+              <br />
+              <p>
+                Full stack developer{' '}
+                <span id='headerDesc'>
+                  passionate about making easy-to-use, accessible and responsive
+                  user interfaces
+                </span>
+              </p>
+            </Col>
+          </Row>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls='responsive-navbar-nav' id='nav-button' />
